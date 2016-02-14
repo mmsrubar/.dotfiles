cf243124720c36ee22ba9bcc5df46384978e9c4b
Michal Šrubař <msrubar@redhat.com>
2016-02-14 19:49:20 +0100

    XFCE4 Terminal settings

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -12,9 +12,10 @@ fi
 
 # $1 file or dir that will be backed up
 backup() {
+  echo "backup() $1"
   if [ -h $HOME/$1 ];
   then
-    echo "> There already is link $HOME/$1"
+    echo "> $HOME/$1 already is a symbolic link"
     exit 1
   elif [ -f $HOME/$1 ] || [ -d $HOME/$1 ];
   then
@@ -37,4 +38,13 @@ case "$answer" in
   *) backup ".fonts"; 
     link ".fonts"; 
     (cd $HOME/.fonts; sh install.sh) ;; #install
+esac
+
+# XFCE Terminal configuration
+echo -n "Do you want to set your xfce terminal settings? [y/n] (Default: y): "
+read answer
+case "$answer" in 
+  n | no | No | No) ;; # do nothing
+  *) backup ".config/xfce4/terminal"; 
+    link ".config/xfce4/terminal" ;;
 esac
