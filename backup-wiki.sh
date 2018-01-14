dacd9f96dce3dbfcb7468fb7cffa7f5472891046
Michal Šrubař <mmsrubar@gmail.com>
2018-01-14 08:50:41 +0100

    Dokuwiki server is now defined through /etc/hosts and ssh config

--- backup-wiki.sh
+++ backup-wiki.sh
@@ -2,15 +2,17 @@
 #
 # Create backup of my personal wiki, downloaded it and all that from an external
 # server using SSH. A password less access to the server over SSH is expected.
+#
 
-SERVER=192.168.0.42
-USER=msrubar
+SERVER=dokuwiki               # defined in /etc/hosts and ssh config
 
 DIR=/var/www/personal_wiki/   # dir to backup
 DATE=`date +%F`               # get current date
 NAME=personal-wiki            # basic name of the backup file
 FILE=$NAME-$DATE.tar.bz2      # backup file
 
-ssh $USER@$SERVER sudo tar cvjf $FILE $DIR
-scp $USER@$SERVER:~/$FILE .
-ssh $USER@$SERVER rm $FILE
+OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+ssh $OPTIONS $SERVER sudo tar cvjf $FILE $DIR
+scp $OPTIONS $SERVER:~/$FILE .
+ssh $OPTIONS $SERVER rm $FILE
